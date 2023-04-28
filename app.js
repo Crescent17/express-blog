f085763aaddbae3e17504986f4f9ffb804e84dfc
Marat <94042173+Crescent17@users.noreply.github.com>
2023-04-28 11:53:09 +0300

    add post schema, post model, functionality to add and read posts

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const ejs = require("ejs");
-const _ = require("lodash")
+const _ = require("lodash");
+const mongoose = require("mongoose");
 
+mongoose.connect('mongodb://127.0.0.1:27017/postsDB')
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
 
-const posts = []
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -19,8 +19,22 @@ app.listen(3000, function () {
     console.log("Server started on port 3000");
 });
 
+const postSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    }
+});
+const Post = mongoose.model("Post", postSchema);
+
 app.get("/", (req, res) => {
-    res.render("home.ejs", {homeStartingContent: homeStartingContent, posts: posts, _: _});
+    Post.find().then(result => {
+        res.render("home.ejs", {homeStartingContent: homeStartingContent, posts: result, _: _})
+    })
 })
 
 app.get("/about", (req, res) => {
@@ -36,19 +50,26 @@ app.get("/compose", (req, res) => {
 })
 
 app.post("/compose", (req, res) => {
-    let post = {
-        title: req.body.postTitle,
-        body: req.body.postBody
-    }
-    posts.push(post)
+    const title = req.body.postTitle;
+    const body = req.body.postBody;
+    const newPost = new Post({
+        title: title,
+        body: body
+    });
+    newPost.save()
     res.redirect("/")
 })
 
-app.get("/posts/:post", (req, res) => {
-    posts.forEach(post => {
-        if (_.kebabCase(post.title) === _.kebabCase(req.params.post)) {
-            res.render("post.ejs", {title: post.title, body: post.body})
-        }
+app.get("/posts/:postId", (req, res) => {
+    Post.findOne({_id: req.params.postId}).then(post => {
+        res.render("post.ejs", {title: post.title, body: post.body})
+    })
+})
+
+app.post("/", (req, res) => {
+    const postId = req.body.postId
+    Post.findById(postId).then(post => {
+        res.redirect(`/posts/${post._id}`)
     })
 
 })
